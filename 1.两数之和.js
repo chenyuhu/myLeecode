4cbba01bc60563ac7040dff250bf8c1a972327ba
chenfeng <chenfeng@sishuxuefu.com>
2019-06-03 16:43:51 +0800

    两数之和

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -9,6 +9,17 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    
+    let index1 = 0
+    let index2 = 1
+    const array = JSON.parse(JSON.stringify(nums))
+    for (let index = 0; index < nums.length; index++) {
+      array.shift()
+      index1 = index
+      for (let n = 0; n < array.length; n++) {
+        if (array[n] + nums[index1] === target) {
+          index2 = nums.length - array.length + n
+          return [index1,index2]
+        }
+      }
+    }
 };
-
