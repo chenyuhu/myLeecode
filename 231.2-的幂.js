3be91fff20f56de31852d344b8c13805c8267e96
chenfeng <chenfeng@sishuxuefu.com>
2019-07-11 09:45:22 +0800

    2的幂

---
+++ 231.2-的幂.js
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode.cn id=231 lang=javascript
+ *
+ * [231] 2的幂
+ */
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+  let str = n.toString(2)
+  return str.lastIndexOf('1') === 0
+};
